db7a30c8ead9869b111af0bfe69eba449fa15c04
neverland <chenjiahan.jait@bytedance.com>
2026-07-10 16:18:17 +0800

    chore: migrate tests to Rstest

--- test/basic/index.test.ts
+++ test/basic/index.test.ts
@@ -1,6 +1,6 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@rstest/playwright';
 import { createRsbuild } from '@rsbuild/core';
 import { pluginYaml } from '../../dist';
 
